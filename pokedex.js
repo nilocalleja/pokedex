87c6d88e25455837bf2aef422b5e2e5c59dcdaa8
nilo <nilo.calleja@bootcamp-upgrade.com>
2023-04-11 09:39:32 +0200

    segundo commit

--- pokedex.js
+++ pokedex.js
@@ -21,11 +21,14 @@ const mapeo = (pok) => {
   return mapear;
 };
 
-const pintarPoke = (pok) => {
+const pintarPoke = (pok, search) => {
   const pokeList = document.getElementById("poke-list");
-  pok.forEach((poke) => {
-    const pokeItem = document.createElement("li");
-    pokeItem.innerHTML = `
+  pokeList.innerHTML = "";
+  pok
+    .filter((poke) => poke.name.toLowerCase().includes(search.toLowerCase())) // Filtra los resultados según el texto ingresado
+    .forEach((poke) => {
+      const pokeItem = document.createElement("li");
+      pokeItem.innerHTML = `
         <div class="poke-info">
           <img src="${poke.image}" alt="${poke.name}">
           <div class="poke-details">
@@ -35,14 +38,19 @@ const pintarPoke = (pok) => {
           </div>
         </div>
       `;
-    pokeList.appendChild(pokeItem);
-  });
+      pokeList.appendChild(pokeItem);
+    });
 };
 
 const inicializar = async () => {
   const result = await getDataApi();
   const pokMap = mapeo(result);
-  pintarPoke(pokMap);
+  pintarPoke(pokMap, ""); 
+  const searchInput = document.getElementById("search-input");
+  searchInput.addEventListener("input", (event) => {
+    pintarPoke(pokMap, event.target.value);
+  });
 };
+
 
 inicializar();
